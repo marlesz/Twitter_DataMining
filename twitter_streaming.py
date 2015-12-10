61bc75de8e26ed3c3d7795e6dada275adb5459cc
marlesz <leszczuk.marta@gmail.com>
2015-12-10 10:55:00 +0100

    streaming

---
+++ twitter_streaming.py
@@ -0,0 +1,41 @@
+__author__ = 'ML'
+
+import urllib3
+urllib3.disable_warnings()
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        #print data
+        with open('twitter_data.txt','a') as tf:
+            tf.write(data)
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    #stream.filter(track=['python', 'javascript', 'ruby'])
+    stream.filter(locations=[-122.4440, 47.4792, -122.2421, 47.7592])
+
